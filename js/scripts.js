67f60925b6342fdb01d0281528bfe3871d431343
Elisa Diaz <e.vallsalfaro@gmail.com>
2023-07-13 19:26:01 +0200

    solucion clase

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const textTask = document.getElementById('text-task');
 
+const textWrite = document.getElementById('text-write');
+
 const allButton = document.getElementById('allButton');
 
 const uncompletedButton = document.getElementById('uncompletedButton');
@@ -22,14 +24,18 @@ const tasks = document.getElementById('tasks');
 // Dentro de ese array iréis añadiendo las tareas nuevas y cuando se eliminen se eliminarán del objeto.
 
 // Para que esté claro, el array de tareas y lo que se pinta en pantalla tiene que tener en todo momento la misma información.
-const tasksLists = [];
+let tasksLists = [];
 
 textTask.addEventListener('submit', event => {
-  tasks.textContent = '';
+  // tasks.textContent = '';
+  event.preventDefault();
 
+  const newDiv = document.createElement('div');
   const newInput = document.createElement('input');
+  newInput.type = 'checkbox';
   const newTextTask = document.createElement('span');
   const newButton = document.createElement('button');
+  newButton.textContent = 'x';
 
   const taskObject = [
     {
@@ -39,26 +45,127 @@ textTask.addEventListener('submit', event => {
     }
   ];
 
+  newTextTask.textContent = textWrite.value;
+  textTask.reset();
+
   taskObject.push(tasksLists);
 
-  textTask.target.value;
+  newDiv.append(newInput);
+  newDiv.append(newTextTask);
+  newDiv.append(newButton);
+
+  tasks.append(newDiv);
+
+  newDiv.classList.add('container-task');
 });
 
-// textTask.addEventListener('submit', event => {
-//   tasks.textContent = '';
-//   const newTextTask = document.createElement('span');
+tasks.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    event.target.parentElement.remove();
+  }
+});
 
-//   newTextTask.textContent = event.target.value;
-//   tasks.classList.add('container-task');
+// array de 10 num donde los num no se repitan BINGO
 
-//   tasks.append(checkbox);
-//   tasks.append(newTextTask);
-//   tasks.append(buttonClose);
-// });
+const tasksElement = document.getElementById('tasks');
+const filtersElement = document.getElementById('filters');
+const formElement = document.getElementById('form');
 
-// buttonClose.addEventListener('click', () => {
-//   tasks.remove();
-// });
+const filters = {
+  all: 0,
+  uncompleted: 1,
+  completed: 2
+};
+let allTasks = [];
 
-// - Saber lo que he escrito en el input
-// - Crear
+// Escuchar el evento de envio del formulario
+// Obtener el valor de lo que hay escrito
+// Meter tarea en el objeto de tareas
+// Generar el HTML correspondiente de cada tarea
+// Meter el HTML en el contenedor de tareas
+// Borrar tarea
+// Actualizar tarea
+
+const generateHtmlForTask = task => {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('task');
+  const newCheckbox = document.createElement('input');
+  newCheckbox.type = 'checkbox';
+  newCheckbox.checked = task.completed;
+  const newTask = document.createElement('span');
+  newTask.textContent = task.task;
+  const newButton = document.createElement('button');
+  newButton.textContent = 'X';
+
+  newButton.addEventListener('click', () => deleteTask(task.id));
+  newCheckbox.addEventListener('change', () => completeTask(task.id));
+
+  newDiv.append(newCheckbox, newTask, newButton);
+
+  return newDiv;
+};
+
+const insertTasks = () => {
+  tasksElement.textContent = '';
+  const fragment = document.createDocumentFragment;
+  allTasks.forEach(task => {
+    const newTask = generateHtmlForTask(task);
+    fragment.append(newTask);
+  });
+  tasksElement.append(fragment);
+};
+
+const deleteTask = id => {
+  // Metodo largo
+  // const newTasks = []
+  // for (const task of allTasks) {
+  //   if (task.id !== id){
+  //     newTasks.push(task)
+  //   }
+  // }
+
+  // allTasks = newTasks
+
+  // Metodo corto
+  allTasks = allTasks.filter(task => task.id !== id);
+
+  insertTasks();
+};
+
+const saveTask = task => {
+  allTasks.push(task);
+  insertTasks();
+};
+
+const completeTask = id => {
+  allTasks = allTasks.map(task => {
+    if (task.id === id) {
+      task.complete = !task.completed;
+    }
+    return task;
+  });
+  insertTasks();
+};
+
+const createTask = task => {
+  const newTask = {
+    id: Date.now(),
+    task: task,
+    completed: false
+  };
+  saveTask(newTask);
+};
+
+const filterTasks = filter => {};
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  if (event.target.task.value === '') return;
+  createTask(event.target.task.value);
+  event.target.reset();
+});
+
+filtersElement.addEventListener('click', event => {
+  if (event.target.tagName !== 'BUTTON') return;
+  filterTasks(event.target.dataset.filter);
+});
